7a7ec21464849af1d08953ef2e5956f9f220fea2
Hiram Njogu <bogita155@gmail.com>
2020-03-02 23:05:56 -0600

    Update code

--- src/com/problem/interest/InterestCalculator.java
+++ src/com/problem/interest/InterestCalculator.java
@@ -5,40 +5,73 @@ import java.util.Scanner;
 public class InterestCalculator {
 
 	public static void main(String[] args) {
+
+		switchStatementBasedRun();
 		
+	}
+
+	public static void switchStatementBasedRun() {
 		String algorithmChoice = null;
-		
-		
+
 		do {
-			//Prompt User
+			// Prompt User
 			Scanner in = new Scanner(System.in);
 			System.out.println("Which algorithm would you like to run. ");
 			System.out.println("Static or Regular");
-			
+
 			algorithmChoice = in.next();
-			
-			if("static".equalsIgnoreCase(algorithmChoice)) {
-				//Run static code
+
+			switch (algorithmChoice.toLowerCase()) {
+				case "static":
+					UserData staticUserDataInput = UserDataReader.staticUserDataReader();
+					double staticResult = calculateInterest(staticUserDataInput);
+					System.out.println("Total Interest from static calculator : " + staticResult + "\n\n");
+					break;
+				case "regular":
+					UserDataReader userDataReader = new UserDataReader();
+					UserData userDataInput = userDataReader.userDataReader();
+					double readerResult = calculateInterest(userDataInput);
+					System.out.println("Total Interest from regular calculator : " + readerResult + "\n\n");
+					break;
+				case "stop":
+					System.out.println("\n\n Calculations Done!");
+					break;
+				default:
+					System.out.println("You entered an invalid choice. Try again\n\n");
+			}
+
+		} while (!"stop".equalsIgnoreCase(algorithmChoice) && !"exit".equalsIgnoreCase(algorithmChoice));
+	}
+	
+	public static void ifStatementBasedRun() {
+		String algorithmChoice = null;
+
+		do {
+			// Prompt User
+			Scanner in = new Scanner(System.in);
+			System.out.println("Which algorithm would you like to run. ");
+			System.out.println("Static or Regular");
+
+			algorithmChoice = in.next();
+
+			if ("static".equalsIgnoreCase(algorithmChoice)) {
+				// Run static code
 				// Static Method call example
 				UserData staticUserDataInput = UserDataReader.staticUserDataReader();
 				double staticResult = calculateInterest(staticUserDataInput);
-				System.out.println("Total Interest from static calculator : " + staticResult+"\n\n");
-			}
-			else if("regular".equalsIgnoreCase(algorithmChoice)) {
-				//Run regular code 
+				System.out.println("Total Interest from static calculator : " + staticResult + "\n\n");
+			} else if ("regular".equalsIgnoreCase(algorithmChoice)) {
+				// Run regular code
 				// Initialize example
 				UserDataReader userDataReader = new UserDataReader();
 				UserData userDataInput = userDataReader.userDataReader();
 				double readerResult = calculateInterest(userDataInput);
-				System.out.println("Total Interest from regular calculator : " + readerResult+"\n\n");
-			}else {
+				System.out.println("Total Interest from regular calculator : " + readerResult + "\n\n");
+			} else {
 				System.out.println("You entered an invalid choice. Try again\n\n");
 			}
-			
-		} while( !"stop".equalsIgnoreCase(algorithmChoice) && !"exit".equalsIgnoreCase(algorithmChoice) );
-		
 
-		System.out.println("\n\n Calculations Done!");
+		} while (!"stop".equalsIgnoreCase(algorithmChoice) && !"exit".equalsIgnoreCase(algorithmChoice));
 	}
 
 	public static double calculateInterest(double amount, double interest, int years) {
