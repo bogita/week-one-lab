962d676a66a322413af4823e5e7f8cc31d67c951
Hiram Njogu <bogita155@gmail.com>
2020-03-02 18:24:20 -0600

    Udpate menu & menu runner

--- src/com/labs/foodMenu/Menu.java
+++ src/com/labs/foodMenu/Menu.java
@@ -5,6 +5,9 @@ public class Menu {
 	private String[] beefMenuOptions = { "Beef Meal 1", "Beef Meal 2" };
 	private String[] drinksMenuOptions = { "Drink One", "Drink Two", "Drink Three" };
 
+	/*
+	 * This method will print the different menu lists depending on the selected menu option
+	 * */
 	public void printMenuoptions(int menu) {
 		
 		switch (menu) {
@@ -18,12 +21,22 @@ public class Menu {
 	
 	}
 
+	/*
+	 * We will use this method to print different items in the menu
+	 * It should print the item with index + 1 to the user
+	 * Ex. 1. Beef Meal 1
+	 * Note Beef Meal 1 is in index 1
+	 * */
 	private void printMenuList(String[] menu) {
 		for (int i = 0; i < menu.length; i++) {
 			System.out.println((i + 1) + " " + menu[i]+"\n");
 		}
 	}
 
+	/*
+	 * We will use this method tp print the item user ordered from the menu of choice
+	 * 
+	 * */
 	public void printOrderedMeal(int menu, int item) {
 		if(menu == 1) {
 			System.out.println("Orderd "+beefMenuOptions[item]+" from Beef Menu");
--- src/com/labs/foodMenu/MenuRunner.java
+++ src/com/labs/foodMenu/MenuRunner.java
@@ -6,7 +6,7 @@ public class MenuRunner {
 
 	public static void main(String[] args) {
 		
-		//Add your loop logic to display/read option
+		//Declare your initial values
 		int choice = -1;
 		Menu menu = new Menu();
 		
