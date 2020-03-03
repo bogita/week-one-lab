ee950bed8d5530ca8fe977199194c7d8098658cd
Hiram Njogu <bogita155@gmail.com>
2020-03-02 22:49:39 -0600

    Update code

--- src/com/problem/interest/InterestCalculator.java
+++ src/com/problem/interest/InterestCalculator.java
@@ -5,35 +5,40 @@ import java.util.Scanner;
 public class InterestCalculator {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner in = new Scanner(System.in);
-
-		//Main method calculation
-		System.out.println("Enter Total Amount: ");
-		double amount = in.nextDouble();
-
-		System.out.println("Enter total years : ");
-		int years = in.nextInt();
-
-		System.out.println("Enter monthly interest : ");
-		double interest = in.nextDouble();
-
-		double compoundInterest = calculateInterest(amount, interest, years);
-		System.out.println("Total Interest is : " + compoundInterest);
-
 		
-		// Static Method call example
-		UserData staticUserDataInput = UserDataReader.staticUserDataReader();
-		double staticResult = calculateInterest(staticUserDataInput);
-		System.out.println("Total Interest is : " + staticResult);
-
+		String algorithmChoice = null;
 		
-		// Initialize example
-		UserDataReader userDataReader = new UserDataReader();
-		UserData userDataInput = userDataReader.userDataReader();
-		double readerResult = calculateInterest(userDataInput);
-		System.out.println("Total Interest is : " + readerResult);
+		
+		do {
+			//Prompt User
+			Scanner in = new Scanner(System.in);
+			System.out.println("Which algorithm would you like to run. ");
+			System.out.println("Static or Regular");
+			
+			algorithmChoice = in.next();
+			
+			if("static".equalsIgnoreCase(algorithmChoice)) {
+				//Run static code
+				// Static Method call example
+				UserData staticUserDataInput = UserDataReader.staticUserDataReader();
+				double staticResult = calculateInterest(staticUserDataInput);
+				System.out.println("Total Interest from static calculator : " + staticResult+"\n\n");
+			}
+			else if("regular".equalsIgnoreCase(algorithmChoice)) {
+				//Run regular code 
+				// Initialize example
+				UserDataReader userDataReader = new UserDataReader();
+				UserData userDataInput = userDataReader.userDataReader();
+				double readerResult = calculateInterest(userDataInput);
+				System.out.println("Total Interest from regular calculator : " + readerResult+"\n\n");
+			}else {
+				System.out.println("You entered an invalid choice. Try again\n\n");
+			}
+			
+		} while( !"stop".equalsIgnoreCase(algorithmChoice) && !"exit".equalsIgnoreCase(algorithmChoice) );
+		
 
+		System.out.println("\n\n Calculations Done!");
 	}
 
 	public static double calculateInterest(double amount, double interest, int years) {
